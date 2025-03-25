e39fb68b25433af294c484d5539831a93909847e
Gustavo Pavão <luisgustavopavao@hotmail.com>
2025-03-25 19:56:08 -0300

    update

--- src/components/ProjectShow.vue
+++ src/components/ProjectShow.vue
@@ -10,17 +10,17 @@ const pomonic = {
   link: "https://pomonic-vue.vercel.app/"
 }
 
-const randoMovie = {
-  name: "RandoMovie",
-  description: "Melhor amigo dos indecisos na hora de escolher um filme. Foi construido utilizando:",
-  tech: ['static/img/tech/vue-svgrepo-com.svg',
-    'static/img/tech/css-3-svgrepo-com.svg',
-    'static/img/tech/html-5-svgrepo-com.svg',
-    'static/img/tech/javascript-svgrepo-com.svg',
-    'static/img/tech/python-svgrepo-com.svg',],
-  repository: "https://github.com/gustapavao/randomovie",
-  link: "https://pavao.life/randomovie",
-}
+// const randoMovie = {
+//   name: "RandoMovie",
+//   description: "Melhor amigo dos indecisos na hora de escolher um filme. Foi construido utilizando:",
+//   tech: ['static/img/tech/vue-svgrepo-com.svg',
+//     'static/img/tech/css-3-svgrepo-com.svg',
+//     'static/img/tech/html-5-svgrepo-com.svg',
+//     'static/img/tech/javascript-svgrepo-com.svg',
+//     'static/img/tech/python-svgrepo-com.svg',],
+//   repository: "https://github.com/gustapavao/randomovie",
+//   link: "https://pavao.life/randomovie",
+// }
 
 const portfolio = {
   name: "Portfólio",
@@ -35,16 +35,45 @@ const portfolio = {
   link: "https://portfolio-gustavo-pavaos-projects.vercel.app/"
 }
 
-const projects = {
-  name: "Gestor de Projetos",
-  description: "API que fornece os dados dos meus projetos pessoais, tais como nome, descrição, repositório etc",
+// const projects = {
+//   name: "Gestor de Projetos",
+//   description: "API que fornece os dados dos meus projetos pessoais, tais como nome, descrição, repositório etc",
+//   tech: [
+//     'static/img/tech/java-svgrepo-com.svg',
+//     'static/img/tech/spring.svg',
+//   ],
+//   repository: "https://github.com/gustapavao/personal_projects",
+//   link: null
+// }
+
+const github_findit = {
+  name: "GitHub Find it",
+  description: "Ferramenta que busca repositórios recentes do usuário buscado e mostra o gráfico das linguagens mais usadas",
   tech: [
-    'static/img/tech/java-svgrepo-com.svg',
-    'static/img/tech/spring.svg',
+    'static/img/tech/atom.png',
+    'static/img/tech/css-3-svgrepo-com.svg',
+    'static/img/tech/typescript.png',
+    'static/img/tech/javascript-svgrepo-com.svg',
+    'static/img/tech/tailwind-css-icon-1725660884.png',
   ],
-  repository: "https://github.com/gustapavao/personal_projects",
-  link: null
+  repository: "https://github.com/gustapavao/github_find_it",
+  link: "https://github-find-it.vercel.app/"
 }
+
+const barbearia = {
+  name: "Barbearia Machado",
+  description: "Site feito para a barbearia Machado localizada na minha cidade natal. Pretendo fazer um sistema de agendamento de horários",
+  tech: [
+    'static/img/tech/atom.png',
+    'static/img/tech/css-3-svgrepo-com.svg',
+    'static/img/tech/typescript.png',
+    'static/img/tech/javascript-svgrepo-com.svg',
+    'static/img/tech/tailwind-css-icon-1725660884.png',
+  ],
+  repository: "https://github.com/gustapavao/barbearia_machado",
+  link: "https://barbearia-machado-five.vercel.app/"
+}
+
 </script>
 
 <template>
@@ -57,9 +86,11 @@ const projects = {
   </div>
   <div class="projects">
     <Projects :name=pomonic.name :description=pomonic.description :tech=pomonic.tech :repository=pomonic.repository :link=pomonic.link />
-    <Projects :name=randoMovie.name :description=randoMovie.description :tech=randoMovie.tech :repository=randoMovie.repository :link=randoMovie.link />
+<!--    <Projects :name=randoMovie.name :description=randoMovie.description :tech=randoMovie.tech :repository=randoMovie.repository :link=randoMovie.link />-->
     <Projects :name=portfolio.name :description=portfolio.description :tech=portfolio.tech :repository=portfolio.repository :link=portfolio.link />
-    <Projects :name=projects.name :description=projects.description :tech=projects.tech :repository=projects.repository :link=projects.link />
+<!--    <Projects :name=projects.name :description=projects.description :tech=projects.tech :repository=projects.repository :link=projects.link />-->
+    <Projects :name=github_findit.name :description=github_findit.description :tech=github_findit.tech :repository=github_findit.repository :link=github_findit.link />
+    <Projects :name=barbearia.name :description=barbearia.description :tech=barbearia.tech :repository=barbearia.repository :link=barbearia.link />
   </div>
 </div>
 </template>
